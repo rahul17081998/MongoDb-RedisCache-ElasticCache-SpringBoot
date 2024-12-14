f39f6505cab4b038e7a340efa6a1bae31a1fc17d
Rahul <86316159+rahul17081998@users.noreply.github.com>
2024-12-14 19:30:30 +0530

    Initial commit: Add Spring Boot project for MongoDB, Redis, and ElasticCache

---
+++ src/test/java/com/redis/mySpringProject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.mySpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
